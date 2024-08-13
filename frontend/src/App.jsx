df4fec1f15996895746a83bab8492ef75f6c8a46
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-08-13 19:01:15 +0700

    komit 5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import About from './components/About'
 import Contact from './components/Contact'
-import Experience from './components/Experience'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
